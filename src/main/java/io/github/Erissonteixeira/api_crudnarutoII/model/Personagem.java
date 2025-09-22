9259bc04b0a0a3c9d944b32f3367c263a3f6f2e2
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-22 16:48:41 -0300

    feat: criar entidade Personagem com atributos básicos e suporte a Jutsus

---
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/Personagem.java
@@ -0,0 +1,39 @@
+package io.github.Erissonteixeira.api_crudnarutoII.model;
+
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Data
+@Table(name = "personagens")
+public class Personagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private int vida = 100;
+    @Column(nullable = false)
+    private int chakra = 100;
+    @Transient
+    private Map<String, Jutsu> jutsus = new HashMap<>();
+
+    public Personagem(){
+    }
+
+    public Personagem(String nome, int vida, int chakra){
+        this.nome = nome;
+        this.vida = vida;
+        this.chakra = chakra;
+    }
+    public void adicionarJutsu(Jutsu jutsu){
+        this.jutsus.put(jutsu.getNome(), jutsu);
+    }
+
+
+}
