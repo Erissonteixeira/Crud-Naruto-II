efdc2d4b4968fbbe737c79f9a046486a58a031e9
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-23 10:58:57 -0300

    refactor: tornar relacionamento entre Personagem e Jutsu bidirecional

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/Jutsu.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/Jutsu.java
@@ -2,9 +2,12 @@ package io.github.Erissonteixeira.api_crudnarutoII.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
+@Table(name = "jutsus")
 public class Jutsu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +18,8 @@ public class Jutsu {
     private int dano;
     @Column(nullable = false)
     private int consumoChakra;
+    @ManyToMany(mappedBy = "jutsus")
+    private List<Personagem> personagens = new ArrayList<>();
 
     public Jutsu(){
     }
