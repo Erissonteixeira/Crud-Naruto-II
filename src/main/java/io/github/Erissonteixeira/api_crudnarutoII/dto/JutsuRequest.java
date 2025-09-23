bd1c0785600e583b41113b2695c37c05e4e88ea4
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-23 14:15:07 -0300

    feat: criar JutsuRequest com validações

---
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuRequest.java
@@ -0,0 +1,15 @@
+package io.github.Erissonteixeira.api_crudnarutoII.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class JutsuRequest {
+    @NotBlank(message = "O nome do jutsu não pode estar vazio")
+    private String nome;
+    @Min(value = 1, message = "O dano deve ser maior que 0")
+    private int dano;
+    @Min(value = 1, message = "O consumo de chakra deve ser maior que 0")
+    private int consumoChakra;
+}
