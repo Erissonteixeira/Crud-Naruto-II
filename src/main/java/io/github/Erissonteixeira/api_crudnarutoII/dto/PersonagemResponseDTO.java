1795fd3e537c90ebe0a86b5626f1ffd7c75df66a
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-23 14:07:51 -0300

    feat: criar PersonagemResponseDTO para retorno de API

---
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/PersonagemResponseDTO.java
@@ -0,0 +1,13 @@
+package io.github.Erissonteixeira.api_crudnarutoII.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class PersonagemResponseDTO {
+    private Long id;
+    private String nome;
+    private int vida;
+    private int chakra;
+    private List<JutsuResponse> jutsus;
+}
