726e8158c29a748fa809d12b9518ad150ed88a67
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-23 15:50:53 -0300

    feat: criar JutsuMapper com MapStruct

---
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/mapper/JutsuMapper.java
@@ -0,0 +1,12 @@
+package io.github.Erissonteixeira.api_crudnarutoII.mapper;
+
+import io.github.Erissonteixeira.api_crudnarutoII.dto.JutsuRequestDTO;
+import io.github.Erissonteixeira.api_crudnarutoII.dto.JutsuResponseDTO;
+import io.github.Erissonteixeira.api_crudnarutoII.model.Jutsu;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface JutsuMapper {
+    Jutsu toEntity(JutsuRequestDTO dto);
+    JutsuResponseDTO toDTO(Jutsu jutsu);
+}
