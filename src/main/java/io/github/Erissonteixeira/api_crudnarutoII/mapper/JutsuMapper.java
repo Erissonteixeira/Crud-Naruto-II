f137bfe21ce7126be1f56e61ae3ee3af181785fe
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-25 18:39:58 -0300

    refactor: aprimorar JutsuMapper com mapeamentos explícitos

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/mapper/JutsuMapper.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/mapper/JutsuMapper.java
@@ -4,9 +4,18 @@ import io.github.Erissonteixeira.api_crudnarutoII.dto.JutsuRequestDTO;
 import io.github.Erissonteixeira.api_crudnarutoII.dto.JutsuResponseDTO;
 import io.github.Erissonteixeira.api_crudnarutoII.model.Jutsu;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface JutsuMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "personagens", ignore = true)
     Jutsu toEntity(JutsuRequestDTO dto);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "nome", source = "nome")
+    @Mapping(target = "dano", source = "dano")
+    @Mapping(target = "consumoChakra", source = "consumoChakra")
     JutsuResponseDTO toResponseDTO(Jutsu jutsu);
 }
