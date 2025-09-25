78c2f7542f7350737db1f3120975501cc15095e0
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-25 16:56:45 -0300

    feat: adicionar endpoint GET para listar todos os Personagens

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/controller/PersonagemController.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/controller/PersonagemController.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/personagens")
@@ -25,5 +24,11 @@ public class PersonagemController{
     public ResponseEntity<PersonagemResponseDTO> criar(@RequestBody PersonagemRequestDTO dto){
         PersonagemResponseDTO criado = personagemService.criar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(criado);
+    }
+    @GetMapping
+    @Operation(summary = "Lista todos os personagens")
+    public ResponseEntity<List<PersonagemResponseDTO>> listarTodos(){
+        List<PersonagemResponseDTO> personagens = personagemService.listarTodos();
+        return ResponseEntity.ok(personagens);
     }
 }
