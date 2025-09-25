f43721495639464bf9d1e219cd075095617b9582
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-25 16:30:29 -0300

    refactor: adicionar jutsusMap em Personagem para otimizar manipulação de Jutsus

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/Personagem.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/Personagem.java
@@ -2,9 +2,10 @@ package io.github.Erissonteixeira.api_crudnarutoII.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Data
@@ -27,6 +28,9 @@ public class Personagem {
     )
     private List<Jutsu> jutsus = new ArrayList<>();
 
+    @Transient
+    private Map<String, Jutsu> jutsusMap = new HashMap<>();
+
     public Personagem(){
     }
 
@@ -36,6 +40,28 @@ public class Personagem {
         this.chakra = chakra;
     }
     public void adicionarJutsu(Jutsu jutsu){
+        if(jutsusMap.containsKey(jutsu.getNome())){
+            throw new IllegalArgumentException("Jutsu já adicionado ao personagem!");
+        }
         this.jutsus.add(jutsu);
+        this.jutsusMap.put(jutsu.getNome(), jutsu);
+    }
+
+    public void removerJutsu(String nomeJutsu){
+        Jutsu jutsu = jutsusMap.get(nomeJutsu);
+        if(jutsu == null){
+            throw new IllegalArgumentException("Este personagem não possui o Jutsu informado!");
+        }
+        this.jutsus.remove(jutsu);
+        this.jutsusMap.remove(nomeJutsu);
+    }
+
+    @PostLoad
+    public void carregarMap(){
+        jutsusMap.clear();
+        for(Jutsu j : jutsus){
+            jutsusMap.put(j.getNome(), j);
+        }
     }
 }
+
