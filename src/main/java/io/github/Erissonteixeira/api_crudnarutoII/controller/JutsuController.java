5a5282ecf26412d156437031245c4a783d6b8b4f
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-25 18:09:44 -0300

    feat(controller): adicionar endpoint GET para listar todos os Jutsus

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/controller/JutsuController.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/controller/JutsuController.java
@@ -4,14 +4,13 @@ import io.github.Erissonteixeira.api_crudnarutoII.dto.JutsuRequestDTO;
 import io.github.Erissonteixeira.api_crudnarutoII.dto.JutsuResponseDTO;
 import io.github.Erissonteixeira.api_crudnarutoII.service.JutsuService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jutsus")
@@ -25,5 +24,11 @@ public class JutsuController{
     public ResponseEntity<JutsuResponseDTO> criar(@RequestBody JutsuRequestDTO dto) {
         JutsuResponseDTO criado = jutsuService.criar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(criado);
+    }
+    @GetMapping
+    @Operation(summary = "Lista todos os jutsus")
+    public ResponseEntity<List<JutsuResponseDTO>> listarTodos() {
+        List<JutsuResponseDTO> jutsus = jutsuService.listarTodos();
+        return ResponseEntity.ok(jutsus);
     }
 }
