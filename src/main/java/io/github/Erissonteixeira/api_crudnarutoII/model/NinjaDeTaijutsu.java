ac02d1cdfadea5ce7f5db975bb8ae0e99ace3732
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-26 16:37:50 -0300

    feat: adicionar checagem de derrota em NinjaDeTaijutsu

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/NinjaDeTaijutsu.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/model/NinjaDeTaijutsu.java
@@ -6,42 +6,52 @@ public class NinjaDeTaijutsu extends Personagem implements Ninja {
 
     private final Random random = new Random();
 
-    public NinjaDeTaijutsu(String nome, int vida, int chakra){
+    public NinjaDeTaijutsu(String nome, int vida, int chakra) {
         super(nome, vida, chakra);
     }
 
     @Override
     public void usarJutsu(String nomeJutsu, Personagem alvo) {
-        Jutsu jutsu = getJutsusMap().get(nomeJutsu);
+        if (isDerrotado()) {
+            System.out.println(getNome() + " está derrotado e não pode atacar!");
+            return;
+        }
 
-        if(jutsu == null){
+        Jutsu jutsu = getJutsusMap().get(nomeJutsu);
+        if (jutsu == null) {
             System.out.println(getNome() + " não possui o jutsu " + nomeJutsu);
             return;
         }
 
-        if(getChakra() < jutsu.getConsumoChakra()){
+        if (getChakra() < jutsu.getConsumoChakra()) {
             System.out.println(getNome() + " não tem chakra suficiente para usar " + nomeJutsu);
             return;
         }
 
         setChakra(getChakra() - jutsu.getConsumoChakra());
-
         alvo.setVida(alvo.getVida() - jutsu.getDano());
 
         System.out.println(getNome() + " usou " + nomeJutsu + " causando " + jutsu.getDano() +
                 " de dano em " + alvo.getNome() + ". Chakra restante: " + getChakra());
+
+        if (alvo.isDerrotado()) {
+            System.out.println(alvo.getNome() + " foi derrotado!");
+        }
     }
 
     @Override
     public void desviar(int danoRecebido) {
-
-        boolean sucesso = random.nextBoolean();
-
-        if(sucesso){
+        boolean sucesso = random.nextBoolean(); // 50% chance
+        if (sucesso) {
             System.out.println(getNome() + " conseguiu desviar do ataque!");
         } else {
             setVida(getVida() - danoRecebido);
-            System.out.println(getNome() + " falhou ao desviar e recebeu " + danoRecebido + " de dano. Vida restante: " + getVida());
+            System.out.println(getNome() + " falhou ao desviar e recebeu " + danoRecebido +
+                    " de dano. Vida restante: " + getVida());
+
+            if (isDerrotado()) {
+                System.out.println(getNome() + " foi derrotado!");
+            }
         }
     }
 }
