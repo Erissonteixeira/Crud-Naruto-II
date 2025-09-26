2ec8babafde4e95860559aff30f7917cdc295841
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-26 15:09:57 -0300

    docs: adicionar anotações @Schema para Swagger no JutsuResponseDTO

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuResponseDTO.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuResponseDTO.java
@@ -1,11 +1,16 @@
 package io.github.Erissonteixeira.api_crudnarutoII.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class JutsuResponseDTO {
+    @Schema(description = "ID do jutsu", example = "1")
     private Long id;
+    @Schema(description = "Nome do jutsu", example = "Rasengan")
     private String nome;
+    @Schema(description = "Dano do jutsu", example = "150")
     private int dano;
+    @Schema(description = "Consumo de chakra do jutsu", example = "50")
     private int consumoChakra;
 }
