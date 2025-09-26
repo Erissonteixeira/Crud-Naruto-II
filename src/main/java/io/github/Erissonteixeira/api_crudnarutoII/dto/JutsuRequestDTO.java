cf21632c1dc9c1c65d6d0ad1bf6437dfdb3926fb
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-26 15:09:04 -0300

    docs: adicionar anotações @Schema para Swagger no JutsuRequestDTO

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuRequestDTO.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuRequestDTO.java
@@ -1,5 +1,6 @@
 package io.github.Erissonteixeira.api_crudnarutoII.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -7,9 +8,12 @@ import lombok.Data;
 @Data
 public class JutsuRequestDTO {
     @NotBlank(message = "O nome do jutsu não pode estar vazio")
+    @Schema(description = "Nome do jutsu", example = "Rasengan")
     private String nome;
     @Min(value = 1, message = "O dano deve ser maior que 0")
+    @Schema(description = "Dano do jutsu", example = "150")
     private int dano;
     @Min(value = 1, message = "O consumo de chakra deve ser maior que 0")
+    @Schema(description = "Consumo de chakra do jutsu", example = "50")
     private int consumoChakra;
 }
