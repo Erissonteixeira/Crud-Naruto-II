ef4180f0875f5d6594c38af11dcd6fc0d017dc81
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-26 15:55:18 -0300

    docs: adicionar anotações @Schema no PersonagemResponseDTO

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/PersonagemResponseDTO.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/PersonagemResponseDTO.java
@@ -1,13 +1,19 @@
 package io.github.Erissonteixeira.api_crudnarutoII.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.util.List;
 
 @Data
 public class PersonagemResponseDTO {
+    @Schema(description = "ID do personagem", example = "1")
     private Long id;
+    @Schema(description = "Nome do personagem", example = "Naruto Uzumaki")
     private String nome;
+    @Schema(description = "Vida do personagem", example = "1000")
     private int vida;
+    @Schema(description = "Chakra do personagem", example = "5000")
     private int chakra;
+    @Schema(description = "Lista de jutsus associados ao personagem")
     private List<JutsuResponseDTO> jutsus;
 }
