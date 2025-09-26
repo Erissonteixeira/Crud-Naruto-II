61824434eafbeab2b77f8affcf471a52a789e654
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-26 15:54:35 -0300

    docs: adicionar anotações @Schema no PersonagemRequestDTO

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/PersonagemRequestDTO.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/PersonagemRequestDTO.java
@@ -1,5 +1,6 @@
 package io.github.Erissonteixeira.api_crudnarutoII.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,10 +10,14 @@ import java.util.List;
 @Data
 public class PersonagemRequestDTO {
     @NotBlank(message = "O nome do personagem não pode estar vazio")
+    @Schema(description = "Nome do personagem", example = "Naruto Uzumaki")
     private String nome;
     @Min(value = 0, message = "A vida não pode ser negativa")
+    @Schema(description = "Vida do personagem", example = "1000")
     private int vida;
     @Min(value = 0, message = "O chakra não pode ser negativo")
+    @Schema(description = "Chakra do personagem", example = "5000")
     private int chakra;
+    @Schema(description = "IDs dos jutsus associados", example = "[1, 2]")
     private List<@NotNull(message = "O ID do jutsu não pode ser nulo") Long> jutsusIds;
 }
