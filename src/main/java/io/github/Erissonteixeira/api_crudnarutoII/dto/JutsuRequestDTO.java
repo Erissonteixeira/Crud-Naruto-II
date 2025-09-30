3346e3455066b4cdb1c27437dd81007a14e5c18d
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-30 07:54:13 -0300

    feat: adicionar validação @Min e documentação Swagger para chakraNecessario do Jutsu

--- src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuRequestDTO.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnarutoII/dto/JutsuRequestDTO.java
@@ -7,13 +7,20 @@ import lombok.Data;
 
 @Data
 public class JutsuRequestDTO {
+
     @NotBlank(message = "O nome do jutsu não pode estar vazio")
     @Schema(description = "Nome do jutsu", example = "Rasengan")
     private String nome;
+
     @Min(value = 1, message = "O dano deve ser maior que 0")
     @Schema(description = "Dano do jutsu", example = "150")
     private int dano;
+
     @Min(value = 1, message = "O consumo de chakra deve ser maior que 0")
     @Schema(description = "Consumo de chakra do jutsu", example = "50")
     private int consumoChakra;
+
+    @Min(value = 1, message = "O chakra necessário deve ser maior que 0")
+    @Schema(description = "Chakra necessário do jutsu", example = "50")
+    private int chakraNecessario;
 }
